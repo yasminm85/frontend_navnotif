0367de2a37f991c6599dae3d6c1542940c7499b9
Yasmin <yasminmutiarabintang@gmail.com>
2026-02-05 09:08:42 +0700

    add menu item evp dashboard tindak lanjut

--- src/menu-items/other2.js
+++ src/menu-items/other2.js
@@ -1,5 +1,6 @@
 // src/menu-items/other.js
 import { IconDashboard } from '@tabler/icons-react';
+import FeedbackIcon from '@mui/icons-material/Feedback';
 
 const other2 = {
   id: 'other2-group',
@@ -15,6 +16,15 @@ const other2 = {
     breadcrumbs: false,
     allowedRoles: ['EVP']
   },
+  {
+      id: 'dashboard-tindak-lanjut',
+      title: 'Dashboard Tindak Lanjut',
+      type: 'item',
+      url: '/dashboard-tindak-lanjut',
+      icon: FeedbackIcon,
+      breadcrumbs: false,
+      allowedRoles: ['EVP']
+    },
 
   ]
 };
