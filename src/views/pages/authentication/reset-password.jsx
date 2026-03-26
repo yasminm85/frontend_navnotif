6fa32a3ba788f950dc1d3771b550e494ad8bf17b
Yasmin <yasminmutiarabintang@gmail.com>
2026-03-26 14:59:15 +0700

    fixing api missing

--- src/views/pages/authentication/reset-password.jsx
+++ src/views/pages/authentication/reset-password.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import api from '../../api/axios';
+import api from '../../../api/axios';
 import Swal from "sweetalert2";
 import {
   Box,
