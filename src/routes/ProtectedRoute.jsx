93c09e73681133eb66da27be70a517e41b419c7e
Yasmin <yasminmutiarabintang@gmail.com>
2026-03-27 14:34:30 +0700

    fx

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -2,9 +2,8 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
-  const { user, token, isInitialized } = useSelector((state) => state.auth);
+  const { user, token } = useSelector((state) => state.auth);
 
-  if (!isInitialized) return null; 
   
   // jika belum login
   if (!token) {
